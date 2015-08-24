f0495a641874a82913f0218c544824c717796296
Jordi Anguela <jordi.anguela@gmail.com>
2015-08-24 14:09:07 +0200

    a Product has a Quality value

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,19 +1,22 @@
 describe("Gilded Rose,", function() {
 
   describe("a product", function() {
-
     it("has a name", function() {
       var name = "Crafted beer";
-      var items = [ new Item(name, 0, 0) ];
-      update_quality();
-      expect(items[0].name).toEqual(name);
+      var item = new Item(name, 0, 0);
+      expect(item.name).toEqual(name);
     });
 
     it("has a SellIn value", function() {
       var sellInValue = 10;
-      var items = [ new Item("", sellInValue, 0) ];
-      update_quality();
-      expect(items[0].sell_in).toEqual(sellInValue);
+      var item = new Item("", sellInValue, 0);
+      expect(item.sell_in).toEqual(sellInValue);
+    });
+
+    it("has a Quality value", function() {
+      var qualityValue = 20;
+      var item = new Item("", 0, qualityValue);
+      expect(item.quality).toEqual(qualityValue);
     });
   });
 });
