675a7a639ed2aefcf7d407cedf88bb4b1c0c643b
Jordi Anguela <jordi.anguela@gmail.com>
2015-08-24 13:58:49 +0200

    a Product has a SellIn value

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,9 +4,16 @@ describe("Gilded Rose,", function() {
 
     it("has a name", function() {
       var name = "Crafted beer";
-        items = [ new Item(name, 0, 0) ];
+      var items = [ new Item(name, 0, 0) ];
       update_quality();
       expect(items[0].name).toEqual(name);
+    });
+
+    it("has a SellIn value", function() {
+      var sellInValue = 10;
+      var items = [ new Item("", sellInValue, 0) ];
+      update_quality();
+      expect(items[0].sell_in).toEqual(sellInValue);
     });
   });
 });
