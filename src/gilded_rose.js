93ebf97331c27d6daa66a0933b647a7d88b7f249
Jordi Anguela <jordi.anguela@gmail.com>
2015-08-26 15:39:00 +0200

    invert negated if

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -49,11 +49,10 @@ function _processQuality(item) {
 
   if (item.sell_in < 0) {
     if (item.name != ItemTypes.AGED_BRIE) {
-      if (item.name != ItemTypes.CONCERT_PASSES) {
-        decreaseQualityIf(item.name != ItemTypes.LEGENDARY_ITEM, item);
-      }
-      else {
+      if (item.name == ItemTypes.CONCERT_PASSES) {
         item.quality = 0;
+      } else {
+        decreaseQualityIf(item.name != ItemTypes.LEGENDARY_ITEM, item);
       }
     }
     else {
